b18d432bbbf02540570082e1f46b34c326e42eea
quchen <2673052046@qq.com>
2026-06-26 15:40:51 +0800

    new:添加了2021CCPC桂林站 A题代码

---
+++ solutions/CCPC/Regionals/The-2021-CCPC-Regionals/The-2021-CCPC-Guilin-Onsite (XXII Open Cup, Grand Prix of EDG)/A/quchen_A.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+const int N=3e5+10;
+const int mod = 998244353;
+typedef long long ll;
+typedef unsigned long long ull;
+const ll INF = 1e18;
+int n;
+ll x;
+
+void solve()
+{
+    cin>>x;
+    ll ans = x*2-1;
+    cout<<ans<<'\n';
+
+
+}
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    int t=1;
+    cin>>t;
+    while(t--)
+    {
+        solve();
+
+    }
+
+
+    return 0;
+}
